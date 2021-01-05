cb1b021840bda9f61f6631e7e05858aad00a6827
Sahil Sharma <sahil.sharma237333@gmail.com>
2021-01-05 01:05:58 +0000

    created entries inside employees table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store.employees.create(first_name: "Alex", last_name:"John", hourly_rate: 50)
+@store2.employees.create(first_name: "Carl", last_name:"Johnson", hourly_rate:70)
+@store2.employees.create(first_name: "John", last_name:"Marston", hourly_rate:80)
